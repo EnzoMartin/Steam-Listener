83b1ac0b6e106f84c6288c9787fce2758a2050e8
Enzo Martin <enzo.r.martin@gmail.com>
2014-01-19 21:28:55 +0100

    Add current/total count to request based off Issue #1

--- Steam-Listener/Models/AppRequest.cs
+++ Steam-Listener/Models/AppRequest.cs
@@ -8,11 +8,15 @@ namespace Steam_Listener.Models
     public class AppRequest
     {
         public string secret { get; set; }
+        public int current { get; set; }
+        public int total { get; set; }
         public IEnumerable<App> data { get; set; }
 
-        public AppRequest(string s, IEnumerable<App> d)
+        public AppRequest(string s, int current, int total, IEnumerable<App> d)
         {
             this.secret = s;
+            this.current = current;
+            this.total = total;
             this.data = d;
         }
     }
--- Steam-Listener/lib/SteamHttpClient.cs
+++ Steam-Listener/lib/SteamHttpClient.cs
@@ -28,13 +28,14 @@ namespace Steam_Listener.lib
         public void process(List<App> apps)
         {
             int i = 0;
+            int current = 0;
+            int total = apps.Count / max;
             foreach (var grouping in apps.GroupBy(s => ++i / max))
             {
-                var appReq = new AppRequest(secret, grouping.ToList());
+                current++;
+                var appReq = new AppRequest(secret, current, total, grouping.ToList());
                 sendData(JsonConvert.SerializeObject((appReq)));
             }
-
-
         }
 
         public void sendData(string data)
